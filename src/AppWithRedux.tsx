539937a9c96d13c1f3f1190dde2f70ce8d70da33
Nikki Odd <archiektor@gmail.com>
2023-12-24 18:36:12 +0100

    - added redux

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -1,18 +1,12 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import './App.css';
 import {Counter} from './components/Counter/Counter';
 import {SettingsBlock} from './components/SettingsBlock/SettingsBlock';
-import {incrementPreviousStateAC, resetStateAC, setNewStateAC, settingObjStateType, settingsReducer} from './reducers/settingsReducer';
+import {incrementPreviousStateAC, resetStateAC, setNewStateAC, settingObjStateType} from './reducers/settingsReducer';
 import {useDispatch, useSelector} from 'react-redux';
 import {AppRootStateType} from './store/store';
 
 const AppWithRedux = () => {
-
-    // const [settingsObj, dispatchToSettingsObj] = useReducer(settingsReducer, {
-    //     start: 0,
-    //     current: 0,
-    //     end: 5
-    // })
 
     const settingsObj = useSelector<AppRootStateType, settingObjStateType>(state => state.settings);
     const dispatch = useDispatch();
